9fae7ae306827de1a8784e6fd9e254c52a9d6b00
sjhman44 <sjhman44@users.noreply.github.com>
2024-10-30 23:07:15 -0400

    Add in ability to pull data from url / storage

--- FSII/script_FSII.js
+++ FSII/script_FSII.js
@@ -11,51 +11,7 @@ function handleGenerateXMLButtonClick() {
 }
 // Add event listener to the button
 document.getElementById('button-container').addEventListener('click', handleGenerateXMLButtonClick);
-/*
-// default is 
-let targets = '../targetsDEMO.json' 
 
-window.onload = function() {
-    
-    let targetsData, activationData;
-    // Fetch targets.json
-    fetch(targets)
-        .then(response => {
-            if (!response.ok) throw new Error('Cannot load targets.json');
-            return response.json();
-        })
-        .then(data => {
-            targetsData = data;
-            populateDropdowns(data,targetIds);
-            console.log('Targets data loaded:', targetsData); // Debugging statement
-        })
-        .catch(error => console.error('Error fetching targets:', error));
-
-    // Fetch activation.json
-    fetch('../activation.json')
-        .then(response => {
-            if (!response.ok) throw new Error('Cannot load activation.json');
-            return response.json();
-        })
-        .then(data => {
-            activationData = data;
-            populateActivationDropdowns(data,activationIDs);
-            console.log('Activation data loaded:', activationData); // Debugging statement
-        })
-        .catch(error => console.error('Error fetching activations:', error));
-
-    // File upload event listener
-    const fileUpload = document.getElementById('fileUpload');
-    fileUpload.addEventListener('change', function(event) {
-        if (targetsData && activationData) {
-            loadCCLFile(event, targetsData, activationData,targetIds,activationIDs);
-        } else {
-            console.warn('Targets data or activation data is not yet available.'); // Warn if data is missing
-        }
-    });
-};
-
-*/
 window.onload = async function() {
     const targetsData = await getDataFromUrl(); // Await the target data
     const activationData = await getActivationData('../activation.json'); // Await the activation data
