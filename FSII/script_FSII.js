995c2176838730dd3081978fc775eaa23e409652
sjhman44 <sjhman44@users.noreply.github.com>
2024-10-30 23:04:19 -0400

    update panel name variable

--- FSII/script_FSII.js
+++ FSII/script_FSII.js
@@ -2,7 +2,7 @@
 const targetIds = ['target0', 'target1', 'target2', 'target3', 'target4'];
 const activationIDs = ['activation0', 'activation1', 'activation2', 'activation3', 'activation4'];
 const panelType = 'FS2Role'
-const panelName = 'FreespeakII'
+let panelName = 'FreespeakII'
 
 
 // Function to handle the button click
@@ -11,7 +11,7 @@ function handleGenerateXMLButtonClick() {
 }
 // Add event listener to the button
 document.getElementById('button-container').addEventListener('click', handleGenerateXMLButtonClick);
-
+/*
 // default is 
 let targets = '../targetsDEMO.json' 
 
@@ -49,6 +49,34 @@ window.onload = function() {
     fileUpload.addEventListener('change', function(event) {
         if (targetsData && activationData) {
             loadCCLFile(event, targetsData, activationData,targetIds,activationIDs);
+        } else {
+            console.warn('Targets data or activation data is not yet available.'); // Warn if data is missing
+        }
+    });
+};
+
+*/
+window.onload = async function() {
+    const targetsData = await getDataFromUrl(); // Await the target data
+    const activationData = await getActivationData('../activation.json'); // Await the activation data
+
+    if (targetsData) {
+        populateDropdowns(targetsData,targetIds);
+    } else {
+        console.warn('Targets data is not available.');
+    }
+
+    if (activationData) {
+        populateActivationDropdowns(activationData,activationIDs);
+    } else {
+        console.warn('Activation data is not available.');
+    }
+
+    // File upload event listener
+    const fileUpload = document.getElementById('fileUpload');
+    fileUpload.addEventListener('change', function(event) {
+        if (targetsData && activationData) {
+            panelName = loadCCLFile(event, targetsData, activationData,targetIds,activationIDs);
         } else {
             console.warn('Targets data or activation data is not yet available.'); // Warn if data is missing
         }
